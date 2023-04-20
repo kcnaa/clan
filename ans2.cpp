6977bb15981818fb7aec77ca68b62ffdc8876f64
kcnaa <alsk124215545@gmail.com>
2023-04-20 14:06:02 +0800

    init

--- ans2.cpp
+++ ans2.cpp
@@ -6,27 +6,40 @@ int main()
 	char str[100];
 	char str1[100];
 	int err = 0;
-	err = 0;
 	printf("String:");
 	scanf("%s", str);
 	int i = 0;
-	for(i = 0; str[i] != '\n'; i++)
+	for(i = 0; str[i] != '\0'; i++)
 	{
 		if(65 <= (int)(str[i]) && (int)(str[i]) <= 90)
 		{
-			str1[i] = (char)(((int)str[i]) + 32 + 1);
+			if((int)(str[i]) == 90)
+			{
+				str1[i] = (char)97;
+			}
+			else
+			{
+				str1[i] = (char)(((int)str[i]) + 32 + 1);	
+			}
+			
 		}
 		else if(97 <= (int)(str[i]) && (int)(str[i]) <= 122)
 		{
-			str1[i] = (char)(((int)str[i]) - 32 + 1);
+			if((int)(str[i]) == 122)
+			{
+				str1[i] = (char)65;
+			}
+			else
+			{
+				str1[i] = (char)(((int)str[i]) - 32 + 1);
+			}	
 		}
 		else
 		{
-			if(str[i] != '\n')
+			if(str[i] != '\0')
 			{
 				err = 1;
 			}
-			printf("%d is err", i);
 			break;
 		}
 	}
@@ -34,10 +47,8 @@ int main()
 	{
 		printf("%s", str1);
 	}
-	if(err == 1)
+	else
 	{
 		printf("Error!");
-		printf("%d",err);
-		printf(str1);
 	}
 }
