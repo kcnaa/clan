ef3d0b46fec5242d617791680fab6d2095a9ac6e
kcnaa <alsk124215545@gmail.com>
2023-04-20 13:24:26 +0800

    init

--- ans1.cpp
+++ ans1.cpp
@@ -1,7 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 int main()
 {
-	printf("ถู");
+	int numBox[5] = {3,4,6,8,9};
+	int result;
+	int i = 0;
+	int j = 0;
+	for(i = 0; i < 5; i++)
+	{
+		for(j = 0; j < 5; j++)
+		{
+			if(i == j)
+			{
+				continue;
+			}
+			else
+			{
+				result = numBox[i] * 10 + numBox[j];
+				printf("%d\n",result);
+			}
+		}
+	}
 } 
